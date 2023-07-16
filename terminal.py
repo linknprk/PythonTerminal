c446bd86af18179f93b515bc8eeec422bbd88a30
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-16 13:03:11 -0400

    Create terminal.py

---
+++ terminal.py
@@ -0,0 +1,8 @@
+from os import * 
+from tkinter import *
+terminal Tk(className="Terminal")
+# set window size
+terminal.geometry("400x200")
+#Set window colour
+terminap.configure(bg='black')
+terminal.mainloop()
