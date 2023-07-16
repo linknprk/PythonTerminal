c37d3f18c2977345ca0eacca1fc5d5678c8a84fb
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-15 23:03:28 -0400

    Create Login.py

---
+++ Login.py
@@ -0,0 +1,37 @@
+import os 
+def user():
+    tries = 0
+    if os.path.exists("username.txt"):
+        username = open("username.txt", "r")
+        Username = input("Enter your username ")
+        if Username == username.read():
+            password = open("password.txt", "r")
+            Password = input("Enter your password ")
+            if Password == password.read():
+                username = open("username.txt", "r")
+                print("Welcome Back ", username.read())
+                username.close()
+            else:t
+                if not tries == 3:
+                    print("Incorrect Password!")
+                    tries = tries+1
+                    user()
+                else:
+                    print("Access Denied")
+        else:
+            if not tries == 3:
+                print("Incorrect Username")
+                tries = tries+1
+                user()
+            else:
+                print("Access Denied!")
+    else:
+        username = input("Create a username > ")
+        password = input("Create a password > ")
+        Password = open("password.txt", "w")
+        Password.write(password)
+        Password.close()
+        Username = open("username.txt", "w")
+        Username.write(username)
+        Username.close()
+user()
