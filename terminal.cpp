12a6566e64e896d0c5bd4ae948cd52bc3ecb9a87
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-16 13:48:02 -0400

    Update and rename terminal.py to terminal.cpp

---
+++ terminal.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  string commands[10] = {"help", "add", "+", "subtract", "-", "multiply", "*", "divide", "/", "echo"}
+  string terminalInput;
+  printf("Enter a command > ");
+  cin >> terminalInput;
+  bool isCommand = false;
+  for(int i = 0; i < 10; i++){
+     if(command = commands[i]){
+       validCommand = true;
+       break;
+     }
+  }
+  if(validCommand == true){
+    if(terminalInput == "add" || trerminalInput == "+"){
+    float num1, num2;
+    printf("Enter the first number > ");
+    cin >> num1;
+    printf("Enter the second number > ");
+    cin >> num2;
+    cout << "The sum is " << num1+num2 << endl;
+    main();
+  }
+  if(termialInput == "subtrract" || terminalInput == "-"){
+    float num1, num2;
+    printf("Enter the first number > ");
+    cin >> num1;
+    printf("Enter the second number > ");
+    cin >> num2;
+    cout << "The result is " << num-num2 << endl;
+    main();
+  }
+  if(terminalInput == "multiply" || terminalInput == "*"){
+    float num1,num2;
+    printf("Enter the first number > ");
+    cin >> num1;
+    printf("Enter the second number > ");
+    cin >> num2;
+    cout << "The result is " << num1*num2 << endl;
+    main();
+  }
+  if(terminalInput == "divide" || terminalInput == "/"){
+    float num1, num2;
+    printf("Enter the first number > ");
+    cin >> num1;
+    printf("Enter the second number > ");
+    cin >> num2;
+    cout << "The result is " << num1/num2 << endl;
+    main();
+  }
+  if(terminalInput == "echo"){
+    string input;
+    printf("Enter something: ");
+    cin >> input;
+    cout << input << endl;
+    main();
+  }
+  if(command == "help"){
+    cout << "The commands are: " << endl;
+    for(int i = 0; i < 10; i++){
+      cout << commands[i];
+      break;
+    }
+    main();
+  }
+}else{
+    cout << "Invalid Command!" << endl;
+    cout << "The commands that exist are: ";
+    for(int i = 0; i < 10; i++){
+      cout << commands[i];
+      break;  
+    }
+    main();
+  }
+}
