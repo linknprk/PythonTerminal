4e077c65deb30c55b5ebb58f103d85dc20b57879
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-16 22:13:27 -0400

    Update terminal.cpp

--- terminal.cpp
+++ terminal.cpp
@@ -1,110 +1,62 @@
 #include <iostream>
 #include <string>
-#include <cstdlib>
+#include <stdlib>
 using namespace std;
 int main(){
-  string commands[12] = {"ipconfig","echo","help", "exit", "add", "subtract", "multiply", "divide", "+", "-", "*", "/", "cls", "ping"};
-  string terminalInput;
-  printf("> ");
-  cin >> terminalInput;
-  for(int i = 0; i < 12; i++){
-    if(terminalInput == commands[i]){
-      if(terminalInput == "exit"){
-        return 0;
-      }
-      if(terminalInput == "help"){
-        for(int i = 0; i < 12; i++){
-          cout << "The commands are: " << endl;
-          cout << commands[i] << endl;
+    string commands[5] = {"help", "exit", "echo", "iscmd", "ping"};
+    string command;
+    printf("Type help to get a list of commands \n");
+    printf("> ");
+    cin >> command;
+    for(int i = 0; i < 5; i++){
+        if(command == commands[i]){
+            if(command == "help"){
+                for(int i = 0; i < 5; i++){
+                    cout << commands[i] << endl;
+                }
+                main();
+            }
+            if(command == "exit"){
+                string leave;
+                printf("Are you sure you want to leave? Y/N? ");
+                cin >> leave;
+                if(leave == "Y" || leave == "y"){
+                    return 0;
+                }else{
+                    main();
+                }
+            }
+            if(command == "echo"){
+                string userInput;
+                printf("Enter > ");
+                cin >> userInput;
+                cout << userInput << endl;
+                main();
+            }
+            if(command == "iscmd"){
+                string cmd;
+                printf("Enter a command > ");
+                cin >> cmd;
+                for(int i = 0; i < 5; i++){
+                    if(cmd == commands[i]){
+                        cout << "true" << endl;
+                        break;
+                    }
+                    if(cmd != commands[i]){
+                        cout << "false" << endl;
+                        break;
+                    }
+                }
+                main();
+            }
+            if(command == "ping"){
+                string request;
+                printf("Enter the URL/IP Address > ");
+                cin >> request;
+                string cmd = request;
+                system("ping " + cmd);
+                main();
+            }
         }
-        main();
-      }
-      if(terminalInput == "echo"){
-        string userInput;
-        printf("Enter something: ");
-        cin >> userInput;
-        cout << userInput;
-        main();
-      }
-      if(terminalInput == "add" || terminalInput == "+"){
-        float num1,num2;
-        printf("Enter the first number > ");
-        cin >> num1;
-        printf("Enter the second number > ");
-        cin >> num2;
-        cout << "The sum is " << num1+num2 << endl;
-        main();
-      }
-      if(terminalInput == "subtract" || terminalInput == "-"){
-        float num1,num2;
-        printf("Enter the first > ");
-        cin >> num1;
-        printf("Enter the second number > ");
-        cin >> num2;
-        cout << "The result is " << num1-num2 << endl;
-        main();
-      }
-      if(terminalInput == "multiply" || terminalInput == "*"){
-        float num1,num2;
-        printf("Enter the first number > ");
-        cin >> num1;
-        printf("Enter the second number > ");
-        cin >> num2;
-        cout << "The result is " << num1*num2 << endl;
-        main();
-      }
-      if(terminalInput == "divide" || terminalInput == "/"){
-        float num1,num2;
-        printf("Enter the first number ");
-        cin >> num1;
-        printf("Enter the second number ");
-        cin >> num2;
-        cout << "The result is " << num1/num2 << endl;
-        main();
-      }
-      if(terminalInput == "exit"){
-        return 0;
-      }
-      if(terminalInput == "cls"){
-        system("cls");
-        main();
-      }
-      if(terminalInput == "ping"){
-        string request;
-        int amount;
-        printf("Enter a server/IP Address to ping >  ");
-        cin >> request;
-        printf("Enter amount of packets you went to send> > ");
-        cin >> packets;
-        if(packets > 6550){
-          cout << "You can send " << packets << " you can only send 6550 packets" << endl;
-          main();
-        }else{
-          string command = "ping " + request + "-n" + to_string(packets);
-          system(command.c_str());
-          main();
-        }
-      }
-      if(terminalInput == "ipconfig"){
-        string option;
-        print("Enter(/? etc) > ")
-        cin >> option;
-        if(option == ""){
-          system("ipconfig");
-          main();
-        }else{
-          string command = "ipconfig /" + option;
-          system(command);
-          main();
-      }
-    }else{
-      cout << "Invalid Command!" << endl;
-      cout << "The commands are: " << endl;
-      for(int i = 0; i < 12; i++){
-        cout << commands[i] << endl;
-      }
-    main();
     }
-  return 0;
-}
 }
