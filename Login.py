c33b028bf2884793eea0cce13f3352cd12f0a27b
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-17 14:52:02 -0400

    Update Login.py

--- Login.py
+++ Login.py
@@ -11,7 +11,7 @@ def user():
                 username = open("username.txt", "r")
                 print("Welcome Back ", username.read())
                 username.close()
-                import terminal
+                os.system("start terminal++.exe");
             else:
                 if not tries == 3:
                     print("Incorrect Password!")
