28a00fd1d7d89019813a1ab06996443c93bdd1ba
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-17 21:24:54 -0400

    Update changedir.py

--- changedir.py
+++ changedir.py
@@ -1,13 +1,3 @@
-"""
-I'm retarded
-IQ < 15
-I probably can't even get a job
-I think I should kill myself but I don't feel that way
-My family probably won't be sad if I die
-My family will probably throw a party when I die
-I'm too dumb to do anything
-My code probably won't work
-"""
 import os 
 dir = input("Enter a directory > ")
 if os.path.exists(dir):
