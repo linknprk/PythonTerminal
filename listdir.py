59448934d713d203ad2ce26d83c5719657123d0f
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-18 18:05:45 -0400

    Update listdir.py

--- listdir.py
+++ listdir.py
@@ -1,3 +1,9 @@
+"""
+This code:
+1. Loops through files and folders
+2. Checks if the file/folder is a folder
+3. Loops through the directories list and outputs its content
+"""
 import os
 directories = []
 for dirs in os.listdir():
