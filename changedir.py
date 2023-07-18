19936c35739e0d9a00b5542f1e6083a100f3414a
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-17 21:17:39 -0400

    Update changedir.py

--- changedir.py
+++ changedir.py
@@ -1,3 +1,11 @@
+"""
+I'm retarded
+IQ < 15
+I probably can't even get a job
+I think i should kill myself but I don't feel that way
+My family probably won't be sad if I die
+My family will probably throw a party when I die
+"""
 import os 
 dir = input("Enter a directory > ")
 if os.path.exists(dir):
