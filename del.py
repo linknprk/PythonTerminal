67ac1e58d997b99f3f54e7e74bd57404aa080591
pytutorials <124309800+pytutorials@users.noreply.github.com>
2023-07-17 21:28:36 -0400

    Update del.py

--- del.py
+++ del.py
@@ -1,3 +1,11 @@
+"""
+1. Get user to enter a file or directory
+2. Check if the file/folder exists
+3. Check if the file/folder is a file or folder
+4. If the it's a folder delete it
+5. It's a file and get's deleted
+6. The file wasn't found tell the user the file/directory doesn't exist
+"""
 import os
 filedir = input("Enter a file or directory > ")
 if os.path.exists(filedir):
